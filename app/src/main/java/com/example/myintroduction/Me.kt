f7334c174e9654f78bc2b128f656adf5e5a0b7a1
Aaron Fortner <afortner@islander.tamucc.edu>
2024-02-05 23:26:10 -0600

    basics done

--- app/src/main/java/com/example/myintroduction/Me.kt
+++ app/src/main/java/com/example/myintroduction/Me.kt
@@ -1,11 +1,23 @@
 package com.example.myintroduction
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+
 
 class Me : AppCompatActivity() {
+
+    private val backButton: View by lazy { findViewById(R.id.backButton) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_me)
+
+        backButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish() // Close the current activity
+        }
     }
 }
--- app/src/main/java/com/example/myintroduction/Tequila.kt
+++ app/src/main/java/com/example/myintroduction/Tequila.kt
@@ -1,11 +1,21 @@
 package com.example.myintroduction
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class Tequila : AppCompatActivity() {
+
+    private val backButton: View by lazy { findViewById(R.id.backButton) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tequila)
+
+        backButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish() // Close the current activity
+        }
     }
 }
